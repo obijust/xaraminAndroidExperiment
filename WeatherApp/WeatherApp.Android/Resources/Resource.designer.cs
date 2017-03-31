763bfdd5219c841797a1f9ac711b1e0fdc6573c8
Jonathan L. Meek <lemuelmeek@gmail.com>
2017-03-31 18:24:53 -0400

    an inital start on learning Xaramin

--- WeatherApp/WeatherApp.Android/Resources/Resource.designer.cs
+++ WeatherApp/WeatherApp.Android/Resources/Resource.designer.cs
@@ -60,8 +60,20 @@ namespace WeatherApp.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int GetWeatherButton = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int ZipCodeEntry = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int ZipCodeLabel = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int ZipCodeSearchLabel = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int relativeLayout1 = 2131034112;
 			
 			static Id()
 			{
---
+++ WeatherApp/WeatherApp/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var clientResponse = await client.GetAsync(queryString);
+
+            dynamic data = null; 
+            if(clientResponse != null)
+            {
+                string jsonData = clientResponse.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(jsonData);
+            }
+            return data; 
+        }
+    }
+}
